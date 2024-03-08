5c99c30f9adc4094686d1bccbcc313f762e79f51
T9404 <ghubman1@gmail.com>
2024-03-09 02:56:36 +0700

    delete unused imports from tests

--- payment-gate/src/test/java/com/academy/fintech/pg/integration/CreatePaymentIntegrationTest.java
+++ payment-gate/src/test/java/com/academy/fintech/pg/integration/CreatePaymentIntegrationTest.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
